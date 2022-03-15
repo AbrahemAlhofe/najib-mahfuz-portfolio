d726e2f5d79202749b5f43430b82a4f4243cfadc
Abrahem Alhofe <alhofeabrahem88@gmail.com>
2022-03-15 14:49:15 +0200

    feat( sections ): add ( My Works ) section
        - add the library of the books
        - add all the shades
        - add all the titles
        - use ( postcss-for ) plugin
        - use ( postcss-random ) plugin
        - add najib mahfuz's figure 3

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,9 @@ module.exports = {
 
     plugins: [
         require("postcss-nested"),
-        require("postcss-media-minmax")
+        require("postcss-media-minmax"),
+        require("postcss-for"),
+        require("postcss-random")
     ]
 
 }
