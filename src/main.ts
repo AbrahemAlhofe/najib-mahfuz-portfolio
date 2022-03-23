d0c9437d26bd8d572b2f04335d2b812b052f4009
Abrahem Alhofe <alhofeabrahem88@gmail.com>
2022-03-23 21:37:18 +0200

    feat( sections / main ): add load animations and parallax animation to ( main ) figure
        - install ( gsap )
        - add mask to ( pronoun )
            | to hide ( pronoun ) after some scrolling
        - add ready state animation to all texts
        - add parallax animation to ( pronoun )
        - add padding to ( main ) section

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
 import 'reset-css'
 import './style.css'
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+document.addEventListener("readystatechange", () => {
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+  gsap.from([".figure#main #Frame", ".figure#main #FirstName", ".figure#main #SecondName", ".figure#main #Pronoun"], {
+    yPercent: 50,
+    opacity: 0.2,
+    duration: 1.5
+  });
+
+  gsap.to(".figure#main #Pronoun", {
+    scrollTrigger: {
+      scrub: 0.5,
+      end: "+=1600"
+    },
+    y: 1500,
+  });
+
+});
+
+// Register Plugins
+gsap.registerPlugin(ScrollTrigger);
