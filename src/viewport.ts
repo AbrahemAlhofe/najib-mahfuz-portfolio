81115f2efee6e4c2d5bb1f5210611c250dad840c
Abrahem Alhofe <alhofeabrahem88@gmail.com>
2022-06-12 15:10:37 +0200

    fix( library ): use "touch" events to detect "swipe" events
        some mobile devices doesn't support "wheel" event,
        so using "touch" events will make us be able to detect "swipe"
        events

--- src/viewport.ts
+++ src/viewport.ts
@@ -9,6 +9,8 @@ type TEvents = {
     ['swipe:horizontal'] : any
 };
 
+enum Directions { Up, Down, Left, Right }
+
 // @ts-ignore
 export default class ViewportAggregator implements Emitter<TEvents> {
 
@@ -63,6 +65,71 @@ export default class ViewportAggregator implements Emitter<TEvents> {
         }
     
         document.addEventListener("wheel", onWheelEventHandler);
+
+        (() => {
+
+          let y: number = 0;
+          let x: number = 0;
+          let deltaY: number = 0;
+          let deltaX: number = 0;
+          let direction: Directions | null = null;
+  
+          document.addEventListener("touchstart", (event) => {
+  
+            for ( let touch of event.changedTouches ) {
+              
+              y = touch.screenY;
+              x = touch.screenX;
+  
+            }
+  
+          })
+  
+          document.addEventListener("touchend", (event) => {
+  
+            for ( let touch of event.changedTouches ) {
+              
+
+              deltaY = touch.screenY - y;
+              deltaX = touch.screenX - x;
+
+              if ( Math.abs( deltaY ) > Math.abs( deltaX ) ) {
+
+                direction = deltaY > 0 ? Directions.Up : Directions.Down;
+  
+              } 
+
+              if ( Math.abs( deltaY ) < Math.abs( deltaX ) ) {
+
+                direction = deltaX > 0 ? Directions.Left : Directions.Right;
+  
+              } 
+              
+              console.table({ deltaX, deltaY, direction })
+              
+              if ( deltaX === 0 || deltaY === 0 ) direction = null;
+  
+            }
+  
+          })
+  
+          document.addEventListener("touchend", () => {
+  
+            if ( direction !== null ) {
+  
+              if ( direction === Directions.Up ) this.#emitter.emit("swipe:down");
+              if ( direction === Directions.Down ) this.#emitter.emit("swipe:up");
+              if ( direction === Directions.Up || direction === Directions.Down ) this.#emitter.emit("swipe:vertical");
+  
+              if ( direction === Directions.Left ) this.#emitter.emit("swipe:right");
+              if ( direction === Directions.Right ) this.#emitter.emit("swipe:left");
+              if ( direction === Directions.Left || direction === Directions.Right ) this.#emitter.emit("swipe:horizontal");
+  
+            }
+  
+          })
+
+        })()
 
     }
 
